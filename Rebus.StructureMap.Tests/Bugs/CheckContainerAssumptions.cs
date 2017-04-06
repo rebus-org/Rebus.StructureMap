8c7dc359554f41ce0f390c0768c97e1fed98fdac
Mogens Heller Grabe <mookid8000@gmail.com>
2017-04-06 08:18:30 +0200

    add another test

--- Rebus.StructureMap.Tests/Bugs/CheckContainerAssumptions.cs
+++ Rebus.StructureMap.Tests/Bugs/CheckContainerAssumptions.cs
@@ -8,14 +8,31 @@ namespace Rebus.StructureMap.Tests.Bugs
     public class CheckContainerAssumptions
     {
         [Test]
-        public void WorksAsAdvertised()
+        public void WorksAsAdvertised_1()
         {
             var container = new Container();
 
             container.Configure(c =>
             {
                 c.For<IAmEmptyAndGeneric<ISomeInterface>>().Use<FirstClass>().Transient();
-                //c.For<IAmEmptyAndGeneric<SomeMessage>>().Use<SecondClass>().Transient();
+                c.For<IAmEmptyAndGeneric<SomeMessage>>().Use<SecondClass>().Transient();
+            });
+
+            var handlers = container.GetAllInstances<IAmEmptyAndGeneric<SomeMessage>>().ToArray();
+
+            Assert.That(handlers.Length, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void WorksAsAdvertised_2()
+        {
+            var container = new Container();
+
+            container.Configure(c =>
+            {
+                // two handler types handling same interface
+                c.For<IAmEmptyAndGeneric<ISomeInterface>>().Use<FirstClass>().Transient();
+                c.For<IAmEmptyAndGeneric<ISomeInterface>>().Use<ThirdClass>().Transient();
             });
 
             var handlers = container.GetAllInstances<IAmEmptyAndGeneric<SomeMessage>>().ToArray();
@@ -26,6 +43,8 @@ namespace Rebus.StructureMap.Tests.Bugs
         class FirstClass : IAmEmptyAndGeneric<ISomeInterface> { }
 
         class SecondClass : IAmEmptyAndGeneric<SomeMessage> { }
+
+        class ThirdClass : IAmEmptyAndGeneric<ISomeInterface> { }
 
         public interface ISomeInterface { }
 
